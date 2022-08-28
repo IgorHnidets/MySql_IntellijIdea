038eb2205dc4f03d2b538846d46f0138407e57dc
IgorHnidets <igora6669@gmail.com>
2022-08-28 10:05:43 +0300

    commit1

--- src/homework/Lesson1_Вступ_до_MySql/Homework/library.sql
+++ src/homework/Lesson1_Вступ_до_MySql/Homework/library.sql
@@ -1,18 +1,18 @@
--- create database if not exists library;--
+create database if not exists library;--
 use library;
 
 select * from books;
 
--- create table books(
--- name VARCHAR(120) not null,
--- description VARCHAR(120) default("description missing"),
--- date VARCHAR(15) not null default("2018-01-01"),
--- author VARCHAR(60) not null,
--- category VARCHAR(30) not null
--- );
+create table books(
+name VARCHAR(120) not null,
+description VARCHAR(120) default("description missing"),
+date VARCHAR(15) not null default("2018-01-01"),
+author VARCHAR(60) not null,
+category VARCHAR(30) not null
+);
 
--- insert into books(name,description,date,author,category)
--- values
--- ("Big Encyclopedya",default,default,"Oxford","scientific"),
--- ("Physics","about Physics","1992","USSR","scientific"),
--- ("Chemistry",default,"2001","Ukraine","scientific");
+insert into books(name,description,date,author,category)
+values
+("Big Encyclopedya",default,default,"Oxford","scientific"),
+("Physics","about Physics","1992","USSR","scientific"),
+("Chemistry",default,"2001","Ukraine","scientific");
